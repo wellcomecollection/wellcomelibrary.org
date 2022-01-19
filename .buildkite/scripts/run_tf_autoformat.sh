b935baaf64ec97a5a263210fa78adc8dc414ff28
Alex Chan <alex@alexwlchan.net>
2022-01-19 11:44:38 +0000

    Use bash for the autoformatting, not Python

---
+++ .buildkite/scripts/run_tf_autoformat.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+
+ROOT=$(git rev-parse --show-toplevel)
+
+# Run the Terraform autoformatting
+docker run --tty --rm \
+  --volume "$ROOT":/repo \
+  --workdir /repo \
+  760097843905.dkr.ecr.eu-west-1.amazonaws.com/hashicorp/terraform:light fmt -recursive
+
+# If there are any changes, push to GitHub immediately and fail the
+# build.  This will abort the remaining jobs, and trigger a new build
+# with the reformatted code.
+has_changes=0
+git diff --exit-code || rc=$?
+
+if (( has_changes == 0 ))
+then
+  echo "*** There were no changes from auto-formatting"
+  exit 0
+else
+  echo "*** There were changes from formatting, creating a commit"
+
+  git config user.name "Buildkite on behalf of Wellcome Collection"
+  git config user.email wellcomedigitalplatform@wellcome.ac.uk
+  git remote add ssh-origin "$BUILDKITE_REPO"
+
+  git fetch ssh-origin
+  git checkout --track "ssh-origin/$BUILDKITE_BRANCH"
+
+  git add --verbose --update
+  git commit -m Apply auto-formatting rules
+  git push ssh-origin "HEAD:$BUILDKITE_BRANCH"
+
+  # We exit here to fail the build, so Buildkite will skip to the next
+  # build, which includes the autoformat commit.
+  exit 1
+fi
