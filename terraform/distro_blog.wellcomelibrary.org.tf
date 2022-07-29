3d470a5e407c4bc15f4d128880449cf6dd2752a9
Alex Chan <alex@alexwlchan.net>
2022-07-29 13:30:07 +0100

    this domain no longer goes anywhere

--- terraform/distro_blog.wellcomelibrary.org.tf
+++ terraform/distro_blog.wellcomelibrary.org.tf
@@ -44,8 +44,6 @@ resource "aws_route53_record" "blog-prod" {
   zone_id = data.aws_route53_zone.zone.id
   name    = "blog.wellcomelibrary.org"
   type    = "CNAME"
-  // This is the previous on-premise address, if required for easy roll-back
-  //records = ["bloglibrary.wpengine.com"]
   records = [module.wellcomelibrary_blog-prod.distro_domain_name]
   ttl     = "60"
 
