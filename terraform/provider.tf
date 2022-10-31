f4b13d6710eeeef00b5950dc3d500ec070c06f73
Alex Chan <alex@alexwlchan.net>
2022-10-31 13:32:41 +0000

    add some missing terraform tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,21 @@
+locals {
+  default_tags = {
+    TerraformConfigurationURL = "https://github.com/wellcomecollection/wellcomelibrary.org/tree/main/terraform"
+    Department                = "Digital Platform"
+    Division                  = "Wellcome Collection"
+    Use                       = "wellcomelibrary.org redirects"
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
+  }
+
+  default_tags {
+    tags = local.default_tags
   }
 }
 
@@ -13,6 +26,10 @@ provider "aws" {
   assume_role {
     role_arn = "arn:aws:iam::760097843905:role/platform-developer"
   }
+
+  default_tags {
+    tags = local.default_tags
+  }
 }
 
 provider "aws" {
@@ -21,5 +38,9 @@ provider "aws" {
 
   assume_role {
     role_arn = "arn:aws:iam::267269328833:role/wellcomecollection-assume_role_hosted_zone_update"
+  }
+
+  default_tags {
+    tags = local.default_tags
   }
 }
